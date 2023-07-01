18a5aa82a8fa0ff2ae691c11587d1c02e55657d2
Rafael Guliev <shikamoto.rg@gmail.com>
2023-07-01 10:13:30 +0300

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Rafael on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
